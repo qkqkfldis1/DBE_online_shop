12501da012f3f182818b7d1f02a1360cdb21f3d7
qkqkfldis1 <youhanlee@kaist.ac.kr>
2017-02-05 10:07:04 -0800

    second

---
+++ cart/cart.py
@@ -0,0 +1,77 @@
+from decimal import Decimal
+from django.conf import settings
+from shop.models import Product
+
+class Cart(object):
+
+    def __init__(self, request):
+        """
+        Initialize the cart
+        :param request:
+        """
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID) ## There can be a various kind of session.  bring session with cart ID
+        if not cart:
+            # save an empty cart in the session
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """
+        Add a product to the cart or update its quantity.
+        :param product:
+        :param quantity:
+        :param update_quantity:
+        :return:
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        # update the session cart
+        self.session[settings.CART_SESSION_ID] = self.cart
+        # mark the session as "modified" to make sure it is saved
+        self.session.modified = True
+
+    def remove(self, product):
+        """
+        Remove a product from the cart.
+        :param product:
+        :return:
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products from the database.
+        :return:
+        """
+        products_ids = self.cart.keys() # cart is a session.
+        # get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=products_ids)
+        for product in products:
+            self.cart[str(product.id)]['product'] = product # set specific product into ID
+            ## not saving product itself, just save ID. If we call the cart, we match the product with ID and yield the item.
+
+        for item in self.cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+        Count all items in the cart.
+        :return:
+        """
+        return sum(item['quantity'] for item in self.cart.values())
