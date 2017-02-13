e07bfb7886648c67c38b0cc0f9d2c13a11a1bc1c
qkqkfldis1 <youhanlee@kaist.ac.kr>
2017-02-13 07:27:18 -0800

    very good

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 # for celery
 from .tasks import order_created
+# for custom admin site
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
+
 
 def order_create(request):
     cart = Cart(request)
@@ -19,10 +25,15 @@ def order_create(request):
             # clear the cart
             cart.clear()
             # launch asynchronous task
-            order_created.delay(order.id)
-            return render(request, 'orders/order/created.html',
-                          {'order': order})
+            order_created.delay(order.id) # set the order in the session
+            request.session['order_id'] = order.id # redirect to the payment
+            return redirect(reverse('payment:process'))
     else:
          form = OrderCreateForm()
-    return render(request, 'orders/order/create.html',
-                  {'cart': cart, 'form': form})
+    return render(request, 'orders/order/create.html', {'cart': cart,
+                                                        'form': form})
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
